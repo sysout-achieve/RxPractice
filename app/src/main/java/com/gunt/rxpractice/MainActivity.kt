04c63fb02ef554a9b51249a9698ca92ae34c8f52
gunt <0408gun@gmail.com>
2021-02-13 19:49:01 +0900

    Rx Practice

--- app/src/main/java/com/gunt/rxpractice/MainActivity.kt
+++ app/src/main/java/com/gunt/rxpractice/MainActivity.kt
@@ -5,30 +5,54 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.gunt.rxpractice.databinding.ActivityMainBinding
+import io.reactivex.Flowable
 import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxjava3.subjects.AsyncSubject
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.reactivestreams.Publisher
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setContentView(binding.root)
 
-        val taskObservable: Observable<Task> =
-            Observable.fromIterable(createTasksList())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+    }
 
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+    fun exampleObserver() {
+        val list: MutableList<Task> = ArrayList()
+        list.add(Task("Take out the trash", true, 3))
+        list.add(Task("Walk the dog", false, 2))
+        list.add(Task("Make my bed", true, 1))
+        list.add(Task("Unload the dishwasher", false, 0))
+        list.add(Task("Make dinner", true, 5))
+        val taskObservable: Observable<Task> =
+            Observable.fromIterable(list)
+                .subscribeOn(Schedulers.io())
+                .filter { it.isComplete }
+                .observeOn(AndroidSchedulers.mainThread())
 
         taskObservable.subscribe(object : Observer<Task> {
             override fun onSubscribe(d: Disposable) {
                 Log.d("TAG", "OnSubscribe : Called!")
+                compositeDisposable.add(d)
             }
 
             override fun onComplete() {
@@ -38,6 +62,7 @@ class MainActivity : AppCompatActivity() {
             override fun onNext(t: Task) {
                 Log.d("TAG", "OnNextThread : ${Thread.currentThread().name}")
                 Log.d("TAG", "OnNext : ${t.description}")
+
             }
 
             override fun onError(e: Throwable) {
@@ -45,4 +70,63 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    fun exampleCreateObserver() {
+        val task = Task("Walk the dog", false, 3)
+
+        val taskObservable = Observable
+            .create(ObservableOnSubscribe<Task> {
+                if (!it.isDisposed) {
+                    it.onNext(task)
+                    it.onComplete()
+                }
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        taskObservable.subscribe(object : Observer<Task>{
+            override fun onComplete() {
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onNext(t: Task) {
+                Log.d("RX"," OnNext : ${t.description}")
+            }
+
+            override fun onError(e: Throwable) {
+            }
+        })
+    }
+
+    fun createListObserver(){
+        val tasks = createTasksList()
+        val tasksObservable = Observable
+            .create(ObservableOnSubscribe<Task> {
+                for (task in tasks){
+                    if (!it.isDisposed) it.onNext(task)
+                }
+                if (!it.isDisposed) it.onComplete()
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        tasksObservable.subscribe(object : Observer<Task>{
+            override fun onComplete() {
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onNext(t: Task) {
+                Log.d("Rx", "List OnNext: ${t.description}")
+            }
+
+            override fun onError(e: Throwable) {
+            }
+        })
+
+    }
+
 }
--- app/src/main/java/com/gunt/rxpractice/Task.kt
+++ app/src/main/java/com/gunt/rxpractice/Task.kt
@@ -6,12 +6,12 @@ data class Task(
     val priority: Int
 )
 
-fun createTasksList(): List<Task> {
-    val tasks: MutableList<Task> = ArrayList()
-    tasks.add(Task("Take out the trash", true, 3))
-    tasks.add(Task("Walk the dog", false, 2))
-    tasks.add(Task("Make my bed", true, 1))
-    tasks.add(Task("Unload the dishwasher", false, 0))
-    tasks.add(Task("Make dinner", true, 5))
-    return tasks
+fun createTasksList(): MutableList<Task> {
+    return mutableListOf(
+        Task("Take out the trash", true, 3),
+        Task("Walk the dog", false, 2),
+        Task("Walk the dog", false, 2),
+        Task("Make my bed", true, 1),
+        Task("Unload the dishwasher", false, 0)
+    )
 }
